facd6eccfc98cd60a27e11b217c688fb542f879e
Alex Amies <alex@alexamies.com>
2026-03-07 18:03:17 +0000

    Fix button order > Top Up <, push cues below top nav

--- ModularSpeaking1.js
+++ ModularSpeaking1.js
@@ -318,8 +318,6 @@ function renderCard() {
       renderCard();
     };
   }
-  navRowBottom.appendChild(topBtn);
-
   // Up
   const upBtn = document.createElement('button');
   upBtn.className = 'nav-btn bottom-btn';
@@ -334,8 +332,6 @@ function renderCard() {
       renderCard();
     };
   }
-  navRowBottom.appendChild(upBtn);
-
   // > (forward)
   const fwdBtn = document.createElement('button');
   fwdBtn.className = 'nav-btn bottom-btn nav-arrow';
@@ -348,6 +344,9 @@ function renderCard() {
     fwdBtn.onclick = navigateForward;
   }
   navRowBottom.appendChild(fwdBtn);
+  navRowBottom.appendChild(topBtn);
+  navRowBottom.appendChild(upBtn);
+  navRowBottom.appendChild(backBtn);
 
   footerEl.appendChild(navRowBottom);
 
